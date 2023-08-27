98ded4d6cc0280bb19b18d64ec748dc8c94f1785
Alexandre Auzel <alexandre.auzel@gmail.com>
2023-08-27 14:23:53 +0200

    add authorization

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler
 from requests import get
 
+def is_authorized(update):
+    CHAT_ID = os.getenv('CHAT_ID')
+    return CHAT_ID == str(update.message.chat_id)
 
 def retrieve_IP_address():
     body = get('http://checkip.dyndns.org').content.decode('utf8')
     ip = body.split('Address:')[1].split('<')[0]
     return ip
 
+async def unauthorized_reply(update):
+    await update.message.reply_text('Vous n\'avez pas les droits')
+
 async def start_callback(update, context):
-    await update.message.reply_text('La commande est /getIp')
+    if is_authorized(update):
+        await update.message.reply_text('La commande est /getIp')
+    else:
+        await unauthorized_reply(update)
 
 async def get_IP_callback(update, context):
-    await update.message.reply_text(f'L\'adresse IP du serveur est: {retrieve_IP_address()}')
+    if is_authorized(update):
+        await update.message.reply_text(f'L\'adresse IP du serveur est: {retrieve_IP_address()}')
+    else:
+        await unauthorized_reply(update)
 
 def main():
     load_dotenv()
